7f9d58c95fa3e22c53e10fddd4e86144f4059cb8
Reece Hart <reecehart@gmail.com>
2013-06-11 15:33:51 -0700

    added load-gene; added schema versionining

--- Makefile
+++ Makefile
@@ -9,6 +9,11 @@ setup: install-perl-modules
 install-perl-modules:
 	./sbin/perl-module-install --install-base ve   Log::Log4perl
 
+develop:
+	python setup.py develop
+
+test:
+	PYTHONPATH=lib/python python setup.py nosetests
 
 
 ############################################################################
@@ -18,7 +23,9 @@ clean:
 	find . -name \*~ -print0 | xargs -0r /bin/rm
 cleaner: clean
 	find . \( -name \*.orig -o -name \*.bak \) -print0 | xargs -0r /bin/rm -v
+	rm -f distribute-*
 cleanest: cleaner
+	find . -name \*.pyc -print0 | xargs -0r rm -f
 	/bin/rm -fr *.egg-info ve
 pristine: cleanest
 	# deleting anything unknown to mercurial, including your
--- lib/python/uta/cli.py
+++ lib/python/uta/cli.py
@@ -2,65 +2,125 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+############################################################################
+
 __doc__ = """UTA -- Universal Transcript Archive
 
 Usage:
   uta ( -h | --help )
   uta --version
-  uta -C CONF [options] create-schema
-  uta -C CONF [options] load-gene eutils
-  uta -C CONF [options] load-transcript gbff     --source=SOURCE
-  uta -C CONF [options] load-transcript seqgene  --source=SOURCE
-
-Global Options:
-  --conf=CONF -C=CONF	Configuration to read (required)
+  uta [options] create-schema [ --drop-current ]
+  uta [options] load-gene FILE
+  uta [options] load-transcripts-gbff FILE
+  uta [options] load-transcripts-seqgene FILE
 
 Options:
-  --source=SOURCE  	Source of data
+  -C CONF, --conf CONF	Configuration to read (required)
+
+Examples:
+1) Drop and create schema in PostgreSQL:
+  /usr/bin/dropdb uta
+  /usr/bin/createdb -O reece uta
+  /usr/bin/psql -d uta -c 'create schema uta'
+  PYTHONPATH=lib/python ./bin/uta -C etc/uta.conf create-schema
+
 """
 
-# eg$ /usr/bin/dropdb uta; /usr/bin/createdb -O reece uta; /usr/bin/psql -d uta -c 'create schema uta'; PYTHONPATH=lib/python ./bin/uta -C etc/uta.conf create-schema
-
-
 ############################################################################
 
+import csv, gzip, ConfigParser, logging
+
+from Bio import SeqIO
 from docopt import docopt
-from ConfigParser import SafeConfigParser
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 import uta
+import uta.models
 
 ############################################################################
 
-def create_schema(opts,cf):
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-    import uta.models
+def create_schema(engine,session,opts,cf):
+    if opts['--drop-current']:
+        session.execute('drop schema if exists '+uta.models.schema_name+' cascade')
+        session.execute('create schema '+uta.models.schema_name)
+        session.execute('alter database uta set search_path = '+uta.models.schema_name)
 
-    engine = create_engine(cf.get('uta','dsn'), echo=True)
-    Session = sessionmaker(bind=engine) 
-    
+    session.commit()
     uta.models.Base.metadata.create_all(engine)
-    
-    # inject the schema version into the database
-    session = Session()
     session.add(uta.models.Meta(
             key='schema_version', value=uta.models.schema_version))
     session.commit()
 
 ############################################################################
 
+def load_gene(engine,session,opts,cf):
+    """
+    import data as downloaded (by you) from 
+    ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/gene_info.gz
+    """
+    import uta.parsers.geneinfo
+    gip = uta.parsers.geneinfo.GeneInfoParser(gzip.open(opts['FILE']))
+    for gi in gip:
+        if gi['tax_id'] != '9606' or gi['Symbol_from_nomenclature_authority'] == '-':
+            continue
+        g = uta.models.Gene(gene_id=gi['GeneID'], gene=gi['Symbol_from_nomenclature_authority'], 
+                            name=gi['Full_name_from_nomenclature_authority'], maploc=gi['map_location'],
+                            chrom=gi['chromosome'], descr=gi['description'])
+        session.add(g)
+        logging.info('loaded gene {g.gene} ({g.name})'.format(g=g))
+    session.commit()
+
+############################################################################
+
+def load_transcripts_gbff(engine,session,opts,cf):
+    """
+    import data as downloaded (by you) from 
+    ftp://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/mRNA_Prot/human.rna.gbff.gz
+    """
+    for rec in SeqIO.parse(gzip.open(opts['FILE']),'genbank'):
+        if not rec.id.startswith('NM_'):
+                continue
+        import IPython; IPython.embed();
+    
+############################################################################
+
+def load_transcripts_seqgene(engine,session,opts,cf):
+    """
+    import data as downloaded (by you) from 
+    ftp://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/mRNA_Prot/human.rna.gbff.gz
+    """
+    for rec in SeqIO.parse(gzip.open(opts['FILE']),'genbank'):
+        if not rec.id.startswith('NM_'):
+                continue
+        import IPython; IPython.embed();
+    
+############################################################################
+
 def run(argv=None):
-    opts = docopt(__doc__, argv=argv, version=uta.__version__)
-
-    cf = SafeConfigParser()
-    cf.readfp( open(opts['--conf']) )
-
     dispatch_table = [
-        ('create-schema', create_schema),
+        ('create-schema', 		create_schema),
+        ('load-gene', 			load_gene),
+        ('load-transcripts-gbff', 	load_transcripts_gbff),
+        ('load-transcripts-seqgene', 	load_transcripts_seqgene),
         ]
 
-    for dte in dispatch_table:
-        if opts[dte[0]]:
-            dte[1](opts,cf)
+    opts = docopt(__doc__, argv=argv, version=uta.__version__)
+
+    logging.basicConfig(level=logging.INFO)
+
+    cf = ConfigParser.SafeConfigParser()
+    cf.readfp( open(opts['--conf']) )
+
+    engine = create_engine(cf.get('uta','dsn')) #, echo=True)
+    Session = sessionmaker(bind=engine) 
+    session = Session()
+
+    sub = None
+    for cmd,func in dispatch_table:
+        if opts[cmd]:
+            sub = func
             break
+    if sub is None:
         raise RuntimeError('No valid actions specified')
+    sub(engine,session,opts,cf)
