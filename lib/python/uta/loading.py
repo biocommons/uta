30a570ef0ebf91a72f5663c053f1d0ae6f80a873
Reece Hart <reecehart@gmail.com>
2013-06-15 21:29:22 -0700

    checkpoint in seqgene parsing; moving on to mappers

--- lib/python/uta/loading.py
+++ lib/python/uta/loading.py
@@ -1,10 +1,10 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import csv, gzip, itertools, logging
-import prettytable
 
 import uta
 import uta.sa_models as usam
+import uta.luts
 
 ############################################################################
 
@@ -90,34 +90,68 @@ def load_transcripts_seqgene(engine,session,opts,cf):
     import data as downloaded (by you) as from
     ftp.ncbi.nih.gov/genomes/MapView/Homo_sapiens/sequence/current/initial_release/seq_gene.md.gz
     """
-    import uta.parsers.seqgene
-
-    o_refseq = session.query(usam.Origin).filter(usam.Origin.name == 'RefSeq').one()
-    o_seqgene = session.query(usam.Origin).filter(usam.Origin.name == 'seqgene').one()
+    def _strand_pm_to_int(s):
+        return None if s is None else 1 if s is '+' else -1 if s is '-' else None
     
-
-    sg_filter = lambda r: r['transcript'].startswith('NM_') and r['feature_type'] in ['CDS','UTR']
-    sgparser = uta.parsers.seqgene.SeqGeneParser(gzip.open(opts['FILE']),
-                                                 filter = sg_filter)
-    slurp = sorted(list(sgparser), 
-                   key = lambda r: (r['transcript'],r['group_label'],r['chr_start'],r['chr_stop']))
-    for k,i in itertools.groupby(slurp, key = lambda r: (r['transcript'],r['group_label'])):
-        recs = list(i)
+    def _seqgene_recs_to_tx_info(ac,assy,recs):
+        ti = {
+            'ac': ac,
+            'assy': assy,
+            'strand': _strand_pm_to_int(recs[0]['chr_orient']),
+            'gene_id': int(recs[0]['feature_id'].replace('GeneID:','')) if 'GeneID' in recs[0]['feature_id'] else None,
+            }
         segs = [ (r['feature_type'],int(r['chr_start'])-1,int(r['chr_stop'])) for r in recs ]
         cds_seg_idxs = [ i for i in range(len(segs)) if segs[i][0] == 'CDS' ]
         # merge UTR-CDS and CDS-UTR exons if end of first == start of second
         # prefer this over general adjacent exon merge in case of alignment artifacts
         # last exon
         ei = cds_seg_idxs[-1]
-        cds_end_i = segs[ei][2]
+        ti['cds_end_i'] = segs[ei][2]
         if ei < len(segs)-1:
             if segs[ei][2] == segs[ei+1][1]:
                 segs[ei:ei+2] = [('M',segs[ei][1],segs[ei+1][2])]
         # first exon
         ei = cds_seg_idxs[0]
-        cds_start_i = segs[ei][1]
+        ti['cds_start_i'] = segs[ei][1]
         if ei > 0:
             if segs[ei-1][2] == segs[ei][1]:
                 segs[ei-1:ei+1] = [('M',segs[ei-1][1],segs[ei][2])]
+        ti['exon_se_i'] = [ s[1:3] for s in segs ]
+        return ti
 
-                
+
+    import uta.parsers.seqgene
+
+    o_refseq = session.query(usam.Origin).filter(usam.Origin.name == 'NCBI RefSeq').one()
+    o_seqgene = session.query(usam.Origin).filter(usam.Origin.name == 'NCBI seq_gene').one()
+
+    sg_filter = lambda r: (r['transcript'].startswith('NM_')
+                           and r['group_label'] == 'GRCh37.p10-Primary Assembly'
+                           and r['feature_type'] in ['CDS','UTR'])
+    sgparser = uta.parsers.seqgene.SeqGeneParser(gzip.open(opts['FILE']),
+                                                 filter = sg_filter)
+    slurp = sorted(list(sgparser), 
+                   key = lambda r: (r['transcript'],r['group_label'],r['chr_start'],r['chr_stop']))
+    for k,i in itertools.groupby(slurp, key = lambda r: (r['transcript'],r['group_label'])):
+        ac,assy = k
+        ti = _seqgene_recs_to_tx_info(ac,assy,list(i))
+
+        resp = session.query(usam.Transcript).filter(usam.Transcript.ac == ac)
+        if resp.count() == 0:
+            t = usam.Transcript(ac = ac, origin = o_refseq, gene_id = ti['gene_id'])
+            session.add(t)
+        else:
+            t = resp.one()
+
+        import IPython; IPython.embed()
+
+        chr_ac = uta.lut.chr_to_NC()
+        es = usam.ExonSet(
+            transcript_id = t.transcript_id,
+            ref_nseq_id = 99,
+            origin_id = o_seqgene,
+            strand = ti['strand'],
+            cds_start_i = ti['cds_start_i'],
+            cds_end_i = ti['cds_end_i'],
+            )
+        import IPython; IPython.embed()
