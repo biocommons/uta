16b9217766f10ebc743f176e60e93ed6f0ccca15
Vincent Fusaro <vincent.fusaro@invitae.com>
2013-10-22 15:49:54 -0700

    added NEFL-dbSNP to the tests

--- tests/test_uta_tools_hgvsmapper.py
+++ tests/test_uta_tools_hgvsmapper.py
@@ -12,6 +12,8 @@ class test_HGVSMapper(unittest.TestCase):
         self.hgvsmapper = HGVSMapper( db = TranscriptDB(),
                                       cache_transcripts = True )
 
+    ########### DNAH11 TESTS ############
+
     def test_hgvs_to_genomic_coords_DNAH11_dbsnp(self):
         tests_fn = 'tests/data/DNAH11-dbSNP.tsv'
         tests_in = csv.DictReader(open(tests_fn,'r'),delimiter='\t')
@@ -61,6 +63,59 @@ class test_HGVSMapper(unittest.TestCase):
                 self.hgvsmapper.hgvsg_to_hgvsc(
                     rec['HGVSg'],self.hgvsparser.parse(rec['HGVSc']).seqref,'GRCh37.p10'),
                 rec['HGVSc'])
+
+    ########### NEFL TESTS ############
+
+    def test_hgvs_to_genomic_coords_NEFL_dbsnp(self):
+        tests_fn = 'tests/data/NEFL-dbSNP.tsv'
+        tests_in = csv.DictReader(open(tests_fn,'r'),delimiter='\t')
+        for rec in tests_in:
+            if rec['id'].startswith('#'):
+                continue
+            if rec['HGVSp'] == '':
+                continue
+            if 'NM_006158.4' in rec['HGVSc']:
+                self.assertEqual(self.hgvsmapper.hgvs_to_genomic_coords(rec['HGVSg'])[:3],
+                                 self.hgvsmapper.hgvs_to_genomic_coords(rec['HGVSc'])[:3] )
+
+    #def test_hgvs_to_genomic_coords_NEFL_hgmd(self):
+    #    tests_fn = 'tests/data/NEFL_HGMD_var.tsv'
+    #    tests_in = csv.DictReader(open(tests_fn,'r'),delimiter='\t')
+    #    for rec in tests_in:
+    #        if rec['id'].startswith('#'):
+    #            continue
+    #        if rec['HGVSp'] == '':
+    #            continue
+    #        if 'NM_006158.4' in rec['HGVSc']:
+    #            self.assertEqual(self.hgvsmapper.hgvs_to_genomic_coords(rec['HGVSg'])[:3],
+    #                             self.hgvsmapper.hgvs_to_genomic_coords(rec['HGVSc'])[:3] )
+
+    def test_hgvsg_to_hgvsc_NEFL_dbsnp(self):
+        tests_fn = 'tests/data/NEFL-dbSNP.tsv'
+        tests_in = csv.DictReader(open(tests_fn,'r'),delimiter='\t')
+        for rec in tests_in:
+            if rec['id'].startswith('#'):
+                continue
+            if rec['HGVSp'] == '':
+                continue
+            if 'NM_006158.4' in rec['HGVSc']:
+                self.assertEqual(
+                    self.hgvsmapper.hgvsg_to_hgvsc(rec['HGVSg'],'NM_006158.4','GRCh37.p10'),
+                    rec['HGVSc'])
+
+    #def test_hgvsg_to_hgvsc_NEFL_hgmd(self):
+    #    tests_fn = 'tests/data/NEFL_HGMD_var.tsv'
+    #    tests_in = csv.DictReader(open(tests_fn,'r'),delimiter='\t')
+    #    for rec in tests_in:
+    #        if rec['id'].startswith('#'):
+    #            continue
+    #        if rec['HGVSp'] == '':
+    #            continue
+    #        if 'NM_006158.4' in rec['HGVSc']:
+    #            self.assertEqual(
+    #                self.hgvsmapper.hgvsg_to_hgvsc(rec['HGVSg'],'NM_006158.4','GRCh37.p10'),
+    #                rec['HGVSc'])
+
 
 if __name__ == '__main__':
     unittest.main()
