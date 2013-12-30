c11ba7d91466bafc3cb4714e4568d45055f67661
Reece Hart <reecehart@gmail.com>
2013-12-30 12:06:32 -0800

    added Apache license and code boilerplate to all source files and scripts

--- lib/python/uta/__init__.py
+++ lib/python/uta/__init__.py
@@ -1,3 +1,19 @@
 import pkg_resources
 __version__ = pkg_resources.require('uta')[0].version
 hg_id = 'OBSOLETE'
+
+## <LICENSE>
+## Copyright 2014 UTA Contributors (https://bitbucket.org/invitae/uta)
+## 
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+## 
+##     http://www.apache.org/licenses/LICENSE-2.0
+## 
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+## </LICENSE>
