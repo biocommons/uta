d5512cf77c762a25b8312324b6acaae3836113cf
Reece Hart <reecehart@gmail.com>
2014-02-01 08:16:31 -0800

    removed fetch-align-tasks

--- uta/cli.py
+++ uta/cli.py
@@ -17,7 +17,6 @@ Usage:
   uta [options] load-txinfo FILE
   uta [options] load-exonsets FILE
   uta [options] align-exons FASTA_DIR
-  uta [options] fetch-align-tasks
   uta [options] load-ncbi-seqgene FILE
   
 Options:
@@ -74,7 +73,6 @@ def run(argv=None):
         ('load-txinfo',                 ul.load_txinfo),
 
         ('align-exons',					ul.align_exons),
-        ('fetch-align-tasks',			ul.fetch_align_tasks),
 
         ('load-ncbi-seqgene',           ul.load_ncbi_seqgene),
 
