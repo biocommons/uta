ec715b764d1b81debb0b47a49b2e64f62be4d1ff
Reece Hart <reecehart@gmail.com>
2014-02-11 10:19:03 -0800

    restructured materialized views; added refresh script

---
+++ sql/refresh-matviews.sql
@@ -0,0 +1,4 @@
+refresh materialized view exon_set_exons_fp_mv;
+refresh materialized view tx_aln_cigar_mv;
+refresh materialized view tx_exon_set_summary_mv;
+refresh materialized view tx_aln_summary_mv;
--- sql/utils.sql
+++ sql/utils.sql
@@ -4,8 +4,8 @@ from information_schema.tables
 where table_schema='uta1';
 
 create or replace view _ddl_drop_views_v as
-select concat('drop view ',table_schema,'.',table_name,' cascade;')
+select concat('drop view if exists ',table_schema,'.',table_name,' cascade;')
   from information_schema.views where table_schema='uta1' and table_name ~ '_(v|dv)$'
 UNION
-select concat('drop table ',table_schema,'.',table_name,' cascade;')
+select concat('drop table if exists ',table_schema,'.',table_name,' cascade;')
   from information_schema.tables where table_schema='uta1' and table_name ~ '_(mv)$' ;
