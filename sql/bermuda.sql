998287bd41b89ad6907e879aecb1070d03a904be
Reece Hart <reecehart@gmail.com>
2014-02-11 16:57:08 -0800

    bermuda: materialize WITH NO DATA

--- sql/bermuda.sql
+++ sql/bermuda.sql
@@ -46,22 +46,9 @@ where TASS.alt_ac ~ '^NC_0000'	-- ~ GRCh37 primary assy
 order by hgnc,tx_ac,alt_ac;
 comment on view bermuda_v is 'the infamous bermuda doc!';
 
-create materialized view bermuda_mv as select * from bermuda_v;
+create materialized view bermuda_mv as select * from bermuda_v WITH NO DATA;
 create view bermuda_pivot as select sb_se_i_eq,sb_status_eq,s_refagree,b_refagree,count(*) as n, count(distinct tx_ac) as n_tx_ac,count(distinct hgnc) as n_hgnc from bermuda_mv group by 1,2,3,4;
 grant select on bermuda_mv to public;
-
-
-create or replace view exon_alignments_v as
-select TES.exon_set_id as tes_exon_set_id,AES.exon_set_id as aes_exon_set_id,
-	   TES.tx_ac as tx_ac,AES.alt_ac as alt_ac,AES.alt_strand,AES.alt_aln_method,
-	   TEX.ord as ord,TEX.exon_id as tx_exon_id,AEX.exon_id as alt_exon_id,
-	   TEX.start_i as tx_start_i,TEX.end_i as tx_end_i, AEX.start_i as alt_start_i,AEX.end_i as alt_end_i,
-	   EA.cigar
-from exon_set TES
-join exon_set AES on TES.tx_ac=AES.tx_ac and TES.alt_aln_method='transcript' and AES.alt_aln_method!='transcript'
-join exon TEX on TES.exon_set_id=TEX.exon_set_id
-join exon AEX on AES.exon_set_id=AEX.exon_set_id and TEX.ord=AEX.ord
-left join exon_aln EA on EA.tx_exon_id=TEX.exon_id and EA.alt_exon_id=AEX.exon_id;
 
 
 create view bermuda_pivot_v as select sb_se_i_eq,sb_status_eq,s_refagree,b_refagree,
