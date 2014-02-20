ecaed110eacdcee280ad3023f1d5c192e8204c30
Reece Hart <reecehart@gmail.com>
2014-02-20 15:49:41 -0800

    commit before rfur-bermuda merge

--- sql/utils.sql
+++ sql/utils.sql
@@ -1,3 +1,19 @@
+create or replace function _create_ddl_make_schema_entities_public_ro(s text)
+returns setof text
+language sql strict immutable as
+$$
+  select concat('grant usage on schema ',s,' to PUBLIC;')
+union all
+  select concat('grant select on ',schemaname,'.',tablename,' to PUBLIC;') from pg_tables where schemaname=s
+union all
+  select concat('grant select on ',schemaname,'.',viewname,' to PUBLIC;') from pg_views where schemaname=s
+union all
+  select concat('grant select on ',schemaname,'.',matviewname,' to PUBLIC;') from pg_matviews where schemaname=s
+union all
+  select concat('grant execute on function ',n.nspname,'.',p.proname,'(',pg_catalog.pg_get_function_arguments(p.oid),') to PUBLIC;' FROM pg_catalog.pg_proc p LEFT JOIN pg_catalog.pg_namespace n ON n.oid = p.pronamespace where n.nspname=s
+;
+$$;
+
 create or replace view _ddl_grant_select_v as 
 select concat('grant select on ',table_schema,'.',table_name,' to uta_public;') 
 from information_schema.tables
