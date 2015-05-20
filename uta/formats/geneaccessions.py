d96384a660914b51a730073fac2ef3954871e909
Reece Hart <reecehart@gmail.com>
2015-05-19 18:29:22 -0700

    major loading overhaul; see uta/loading/Makefile for current process

---
+++ uta/formats/geneaccessions.py
@@ -0,0 +1,41 @@
+import csv
+import recordtype
+
+class GeneAccessions( recordtype.recordtype('GeneAccessions',
+                                            ['hgnc','tx_ac','pro_ac','origin']) ):
+    pass
+
+class GeneAccessionsWriter(csv.DictWriter):
+    def __init__(self,tsvfile):
+        csv.DictWriter.__init__(self,tsvfile, fieldnames=GeneAccessions._fields, delimiter=b'\t', lineterminator="\n")
+        csv.DictWriter.writeheader(self)
+        
+    def write(self,si):
+        self.writerow(si._asdict())
+
+class GeneAccessionsReader(csv.DictReader):
+    def __init__(self,tsvfile):
+        csv.DictReader.__init__(self,tsvfile,delimiter=b'\t')
+        if set(self.fieldnames) != set(GeneAccessions._fields):
+            raise RuntimeError('Format error: expected header with these columns: '+','.join(GeneAccessions._fields) + " but got: "+','.join(self.fieldnames))
+
+    def next(self):
+        d = csv.DictReader.next(self)
+        return GeneAccessions(**d)
+
+
+
+if __name__ == '__main__':
+    tmpfn = '/tmp/geneacs'
+
+    with open(tmpfn,'w') as f:
+        esw = GeneAccessionsWriter(f)
+        for i in range(3):
+            es = GeneAccessions(**dict([(k,k+":"+str(i)) for k in GeneAccessions._fields]))
+            esw.write(es)
+
+
+    with open(tmpfn,'r') as f:
+        esr = GeneAccessionsReader(f)
+        for es in esr:
+            print(es)
--- uta/formats/geneinfo.py
+++ uta/formats/geneinfo.py
@@ -1,26 +1,32 @@
 import csv
 import recordtype
 
+default_sep = ','
+
 class GeneInfo( recordtype.recordtype('GeneInfo',
-                                     ['hgnc','maploc','aliases','type','summary','descr']) ):
+                                     ['hgnc', 'maploc', 'aliases', 'type', 'summary', 'descr']) ):
     pass
 
 class GeneInfoWriter(csv.DictWriter):
-    def __init__(self,tsvfile):
-        csv.DictWriter.__init__(self,tsvfile, fieldnames=GeneInfo._fields, delimiter=b'\t')
+    def __init__(self, tsvfile):
+        csv.DictWriter.__init__(self, tsvfile, fieldnames=GeneInfo._fields, delimiter=b'\t', lineterminator="\n")
         csv.DictWriter.writeheader(self)
         
-    def write(self,si):
-        self.writerow(si._asdict())
+    def write(self, si):
+        d = si._asdict()
+        d['aliases'] = default_sep.join(d['aliases'])
+        self.writerow(d)
 
 class GeneInfoReader(csv.DictReader):
-    def __init__(self,tsvfile):
-        csv.DictReader.__init__(self,tsvfile,delimiter=b'\t')
+    def __init__(self, tsvfile):
+        csv.DictReader.__init__(self, tsvfile, delimiter=b'\t')
         if set(self.fieldnames) != set(GeneInfo._fields):
-            raise RuntimeError('Format error: expected header with these columns: '+','.join(GeneInfo._fields) + " but got: "+','.join(self.fieldnames))
+            raise RuntimeError('Format error: expected header with these columns: ' +
+                               ','.join(GeneInfo._fields) + " but got: " + ','.join(self.fieldnames))
 
     def next(self):
         d = csv.DictReader.next(self)
+        d['aliases'] = d['aliases'].split(default_sep)
         return GeneInfo(**d)
 
 
