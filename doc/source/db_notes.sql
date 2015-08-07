fe0751ae89aa51065a1ef21d68d4f264cd1b131a
Reece Hart <reecehart@gmail.com>
2015-08-07 16:20:20 -0400

    minor doc updates

--- doc/source/db_notes.sql
+++ doc/source/db_notes.sql
@@ -5,3 +5,16 @@ pg_dump -h uta.biocommons.org -U ivpostgres -d $d | gzip -c >|$d.pgd.gz
 
 #restore database
 gzip -cdq dumps/uta_20150702.pgd.gz | psql -h uta.biocommons.org -U uta_admin -d uta_dev -aeE
+
+
+createdb -h uta.invitae.com -U ivpostgres -O uta_admin   uta_dev
+
+# copy schema to uta.biocommons.org
+pg_dump -d uta_dev -n uta_20150729 | psql -h uta.biocommons.org -U uta_admin -d uta_dev
+
+# copy schema in a database with renaming
+existing_schema_name=uta_20150704
+new_schema_name=uta_20150729
+pg_dump -U postgres -d uta_dev -n $existing_schema_name \
+| ../sbin/pg-dump-schema-rename $existing_schema_name $new_schema_name \
+| psql -U postgres -d uta_dev
