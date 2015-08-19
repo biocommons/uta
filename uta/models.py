415196536f9ea2a9415da879768622240e88c6ee
Reece Hart <reecehart@gmail.com>
2015-08-19 12:38:34 -0700

    allow cds_md5 to be null in Transcript model

--- uta/models.py
+++ uta/models.py
@@ -125,7 +125,7 @@ class Transcript(Base):
     hgnc = sa.Column(sa.Text)  # , sa.ForeignKey("gene.hgnc"))
     cds_start_i = sa.Column(sa.Integer) #, nullable=False)
     cds_end_i = sa.Column(sa.Integer) #, nullable=False)
-    cds_md5 = sa.Column(sa.Text, nullable=False, index=True)
+    cds_md5 = sa.Column(sa.Text, index=True)
     added = sa.Column(
         sa.DateTime, default=datetime.datetime.now(), nullable=False)
 
