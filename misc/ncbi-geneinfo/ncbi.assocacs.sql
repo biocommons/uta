53a158a242994db6436233f95582e5ef4e9d2526
Andreas Prlic <andreas.prlic@invitae.com>
2017-09-11 11:55:54 -0700

    adding new ncbi.assocacs table, adding ncbi gene id column to assocacs file

---
+++ misc/ncbi-geneinfo/ncbi.assocacs.sql
@@ -0,0 +1,8 @@
+create table assocacs (
+       hgnc text,
+       tx_ac text,
+       pro_ac text,
+       origin text
+       );
+
+
--- uta/formats/geneaccessions.py
+++ uta/formats/geneaccessions.py
@@ -3,7 +3,7 @@ import recordtype
 
 
 class GeneAccessions(recordtype.recordtype('GeneAccessions',
-                                           ['hgnc', 'tx_ac', 'pro_ac', 'origin'])):
+                                           ['hgnc', 'tx_ac', 'gi_ac', 'pro_ac', 'origin'])):
     pass
 
 
