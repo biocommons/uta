6cb84b11b783a2b3bee1396a376f9c3e115a4b7f
Paige Taylor <paige.taylor@invitae.com>
2024-03-06 15:29:03 -0800

    style(IPVC-2231): black

--- sbin/ncbi_parse_genomic_gff.py
+++ sbin/ncbi_parse_genomic_gff.py
@@ -34,31 +34,43 @@ def parse_gff_record(line: str) -> Optional[GFFRecord]:
     # NC_000001.10	BestRefSeq	exon	12613	12721	.	+	.	ID=exon-NR_046018.2-2;Parent=rna-NR_046018.2;Dbxref=GeneID:100287102,Genbank:NR_046018.2,HGNC:HGNC:37102;gbkey=misc_RNA;gene=DDX11L1;product=DEAD/H-box helicase 11 like 1 (pseudogene);pseudo=true;transcript_id=NR_046018.2
     # NC_000001.10	BestRefSeq	exon	13221	14409	.	+	.	ID=exon-NR_046018.2-3;Parent=rna-NR_046018.2;Dbxref=GeneID:100287102,Genbank:NR_046018.2,HGNC:HGNC:37102;gbkey=misc_RNA;gene=DDX11L1;product=DEAD/H-box helicase 11 like 1 (pseudogene);pseudo=true;transcript_id=NR_046018.2
 
-    fields = line.strip().split('\t')
+    fields = line.strip().split("\t")
     if len(fields) != 9:
-        raise ValueError(f'Expected 9 tab-separated fields, got {len(fields)}')
+        raise ValueError(f"Expected 9 tab-separated fields, got {len(fields)}")
 
     seqid, source, feature, start, end, score, strand, phase, attributes_str = fields
 
-    if feature != 'exon' or not seqid.startswith('NC_'):
+    if feature != "exon" or not seqid.startswith("NC_"):
         return
 
     attributes = {}
-    for attr_str in attributes_str.split(';'):
-        if '=' in attr_str:
-            key, value = attr_str.split('=')
+    for attr_str in attributes_str.split(";"):
+        if "=" in attr_str:
+            key, value = attr_str.split("=")
             attributes[key.lower()] = value
 
-    parent_id = attributes.get('parent')
-    transcript_id = attributes.get('transcript_id')
-    if not transcript_id or (not transcript_id.startswith('NM_') and not transcript_id.startswith('NR_')) or not parent_id:
+    parent_id = attributes.get("parent")
+    transcript_id = attributes.get("transcript_id")
+    if (
+        not transcript_id
+        or (not transcript_id.startswith("NM_") and not transcript_id.startswith("NR_"))
+        or not parent_id
+    ):
         return
     try:
-        exon_number = _get_exon_number_from_id(alignment_id=attributes.get('id'))
+        exon_number = _get_exon_number_from_id(alignment_id=attributes.get("id"))
     except (ValueError, IndexError):
         raise ValueError(f'Failed to parse exon number from {attributes.get("id")}')
 
-    return GFFRecord(seqid=seqid, start=int(start), end=int(end), strand=strand, exon_number=exon_number, parent_id=parent_id, transcript_id=transcript_id)
+    return GFFRecord(
+        seqid=seqid,
+        start=int(start),
+        end=int(end),
+        strand=strand,
+        exon_number=exon_number,
+        parent_id=parent_id,
+        transcript_id=transcript_id,
+    )
 
 
 def _get_exon_number_from_id(alignment_id: str) -> int:
@@ -66,19 +78,19 @@ def _get_exon_number_from_id(alignment_id: str) -> int:
     Pulls the exon number from the alignment id. Expects the id to be in the format
     exon-<transcript_id>-<exon_number>
     """
-    return int(alignment_id.split('-')[-1])
+    return int(alignment_id.split("-")[-1])
 
 
 def parse_gff_file(file_path: str) -> dict[str, List[GFFRecord]]:
     tx_data = defaultdict(list)
-    with gzip.open(file_path, 'rt') as f:
+    with gzip.open(file_path, "rt") as f:
         for line in f:
-            if line.startswith('#'):
+            if line.startswith("#"):
                 continue
             try:
                 record = parse_gff_record(line)
             except ValueError as e:
-                raise Exception(f'Failed at line :{line} with error: {e}')
+                raise Exception(f"Failed at line :{line} with error: {e}")
             if record:
                 tx_data[record.parent_id].append(record)
     return {k: _sort_exons(v) for k, v in tx_data.items()}
@@ -88,14 +100,22 @@ def get_zero_based_exon_ranges(transcript_exons: List[GFFRecord]) -> str:
     """Convert exon ranges to 0-based half-open format"""
     formatted_exons = []
     for ex in transcript_exons:
-        formatted_exons.append(','.join(map(str, (ex.start - 1, ex.end))))
-    return ';'.join(formatted_exons)
+        formatted_exons.append(",".join(map(str, (ex.start - 1, ex.end))))
+    return ";".join(formatted_exons)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Parse GFF file.")
-    parser.add_argument("gff_file", type=argparse.FileType('rb'), help="Path to the gzipped GFF file")
-    parser.add_argument("--prefix", "-p", type=str, default="ncbi-gff", help="Output filename (default: ncbi-gff)")
+    parser.add_argument(
+        "gff_file", type=argparse.FileType("rb"), help="Path to the gzipped GFF file"
+    )
+    parser.add_argument(
+        "--prefix",
+        "-p",
+        type=str,
+        default="ncbi-gff",
+        help="Output filename (default: ncbi-gff)",
+    )
     args = parser.parse_args()
 
     gff_infile = args.gff_file
@@ -105,14 +125,19 @@ if __name__ == "__main__":
 
     transcript_alignments = parse_gff_file(gff_infile)
     logger.info(
-        f"read {len(transcript_alignments)} transcript alignments from {gff_infile}")
+        f"read {len(transcript_alignments)} transcript alignments from {gff_infile}"
+    )
 
     for transcript_exons in transcript_alignments.values():
         exons_se = get_zero_based_exon_ranges(transcript_exons)
         e = transcript_exons[0]
-        es = ExonSet(tx_ac=e.transcript_id, alt_ac=e.seqid, method='splign', strand=e.strand, exons_se_i=exons_se)
+        es = ExonSet(
+            tx_ac=e.transcript_id,
+            alt_ac=e.seqid,
+            method="splign",
+            strand=e.strand,
+            exons_se_i=exons_se,
+        )
         esw.write(es)
     output_file.close()
     os.rename(exonset_outfile + ".tmp", exonset_outfile)
-
-
--- tests/test_ncbi_parse_genomic_gff.py
+++ tests/test_ncbi_parse_genomic_gff.py
@@ -4,7 +4,12 @@ from tempfile import NamedTemporaryFile
 import os
 import gzip
 
-from sbin.ncbi_parse_genomic_gff import GFFRecord, parse_gff_file, parse_gff_record, get_zero_based_exon_ranges
+from sbin.ncbi_parse_genomic_gff import (
+    GFFRecord,
+    parse_gff_file,
+    parse_gff_record,
+    get_zero_based_exon_ranges,
+)
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(CURRENT_DIR)
@@ -12,37 +17,64 @@ BASE_DIR = os.path.dirname(CURRENT_DIR)
 
 def sample_line(**params):
     defaults = {
-        'seqid': 'NC_000001.10',
-        'source': 'BestRefSeq',
-        'feature': 'exon',
-        'start': 11874,
-        'stop': 12227,
-        'score': '.',
-        'strand': '+',
-        'phase': '.',
-        'attributes_str': 'ID=exon-NR_046018.2-1;Parent=rna-NR_046018.2;transcript_id=NR_046018.2'
+        "seqid": "NC_000001.10",
+        "source": "BestRefSeq",
+        "feature": "exon",
+        "start": 11874,
+        "stop": 12227,
+        "score": ".",
+        "strand": "+",
+        "phase": ".",
+        "attributes_str": "ID=exon-NR_046018.2-1;Parent=rna-NR_046018.2;transcript_id=NR_046018.2",
     }
     defaults.update(params)
-    return '\t'.join(map(str, defaults.values())) + '\n'
+    return "\t".join(map(str, defaults.values())) + "\n"
 
 
 class TestGFFParsing(unittest.TestCase):
     def setUp(self):
-        with NamedTemporaryFile(suffix='.gz', delete=False) as temp_gff:
-            with gzip.open(temp_gff.name, 'wt') as f:
+        with NamedTemporaryFile(suffix=".gz", delete=False) as temp_gff:
+            with gzip.open(temp_gff.name, "wt") as f:
                 f.write(
-                    "NC_000001.10\tBestRefSeq\texon\t11874\t12227\t.\t+\t.\tID=exon-NR_046018.2-1;Parent=rna-NR_046018.2;transcript_id=NR_046018.2\n")
+                    "NC_000001.10\tBestRefSeq\texon\t11874\t12227\t.\t+\t.\tID=exon-NR_046018.2-1;Parent=rna-NR_046018.2;transcript_id=NR_046018.2\n"
+                )
                 f.write(
-                    "NC_000001.10\tBestRefSeq\texon\t12613\t12721\t.\t+\t.\tID=exon-NR_046018.2-2;Parent=rna-NR_046018.2;transcript_id=NR_046018.2\n")
+                    "NC_000001.10\tBestRefSeq\texon\t12613\t12721\t.\t+\t.\tID=exon-NR_046018.2-2;Parent=rna-NR_046018.2;transcript_id=NR_046018.2\n"
+                )
                 f.write(
-                    "NC_000001.10\tBestRefSeq\texon\t13221\t14409\t.\t+\t.\tID=exon-NR_046018.2-3;Parent=rna-NR_046018.2;transcript_id=NR_046018.2\n")
+                    "NC_000001.10\tBestRefSeq\texon\t13221\t14409\t.\t+\t.\tID=exon-NR_046018.2-3;Parent=rna-NR_046018.2;transcript_id=NR_046018.2\n"
+                )
             temp_gff.seek(0)
         self.temp_gff = temp_gff
         self.gff_records = [
-                GFFRecord(seqid="NC_000001.10", start=11874, end=12227, strand="+", exon_number=1, parent_id="rna-NR_046018.2", transcript_id="NR_046018.2"),
-                GFFRecord(seqid="NC_000001.10", start=12613, end=12721, strand="+", exon_number=2, parent_id="rna-NR_046018.2", transcript_id="NR_046018.2"),
-                GFFRecord(seqid="NC_000001.10", start=13221, end=14409, strand="+", exon_number=3, parent_id="rna-NR_046018.2", transcript_id="NR_046018.2")
-            ]
+            GFFRecord(
+                seqid="NC_000001.10",
+                start=11874,
+                end=12227,
+                strand="+",
+                exon_number=1,
+                parent_id="rna-NR_046018.2",
+                transcript_id="NR_046018.2",
+            ),
+            GFFRecord(
+                seqid="NC_000001.10",
+                start=12613,
+                end=12721,
+                strand="+",
+                exon_number=2,
+                parent_id="rna-NR_046018.2",
+                transcript_id="NR_046018.2",
+            ),
+            GFFRecord(
+                seqid="NC_000001.10",
+                start=13221,
+                end=14409,
+                strand="+",
+                exon_number=3,
+                parent_id="rna-NR_046018.2",
+                transcript_id="NR_046018.2",
+            ),
+        ]
 
     def tearDown(self):
         os.remove(self.temp_gff.name)
@@ -50,41 +82,55 @@ class TestGFFParsing(unittest.TestCase):
     def test_parse_gff_record(self):
         # Test parsing a single GFF record
         line = sample_line()
-        expected_record = GFFRecord(seqid="NC_000001.10", start=11874, end=12227, strand="+", exon_number=1, parent_id="rna-NR_046018.2", transcript_id="NR_046018.2")
+        expected_record = GFFRecord(
+            seqid="NC_000001.10",
+            start=11874,
+            end=12227,
+            strand="+",
+            exon_number=1,
+            parent_id="rna-NR_046018.2",
+            transcript_id="NR_046018.2",
+        )
         parsed_record = parse_gff_record(line)
         self.assertEqual(parsed_record, expected_record)
 
     def test_parse_gff_record_skips_non_exon_records(self):
         # We exclude non-exon records
-        line = sample_line(feature='pseudogene')
+        line = sample_line(feature="pseudogene")
         expected_record = None
         parsed_record = parse_gff_record(line)
         self.assertEqual(parsed_record, expected_record)
 
     def test_parse_gff_record_skips_non_NC_target(self):
         # We exclude alignments to non-NC chromosomes
-        line = sample_line(seqid='NT_123')
+        line = sample_line(seqid="NT_123")
         expected_record = None
         parsed_record = parse_gff_record(line)
         self.assertEqual(parsed_record, expected_record)
 
     def test_parse_gff_record_skips_missing_transcript_id(self):
         # We exclude alignments missing transcript_id
-        line = sample_line(attributes_str='ID=exon-NR_046018.2-1;transcript_id=NR_046018.2')  # parent missing from attributes
+        line = sample_line(
+            attributes_str="ID=exon-NR_046018.2-1;transcript_id=NR_046018.2"
+        )  # parent missing from attributes
         expected_record = None
         parsed_record = parse_gff_record(line)
         self.assertEqual(parsed_record, expected_record)
 
     def test_parse_gff_record_skips_missing_parent_field(self):
         # We exclude alignments missing a parent field
-        line = sample_line(attributes_str='ID=exon-NR_046018.2-1;Parent=rna-NR_046018.2')  # transcript_id missing from attributes
+        line = sample_line(
+            attributes_str="ID=exon-NR_046018.2-1;Parent=rna-NR_046018.2"
+        )  # transcript_id missing from attributes
         expected_record = None
         parsed_record = parse_gff_record(line)
         self.assertEqual(parsed_record, expected_record)
 
     def test_parse_gff_record_skips_non_NM_NR_transcripts(self):
         # We only care about transcripts that start with NM_ or NR_
-        line = sample_line(attributes_str='ID=exon-NR_046018.2-1;Parent=rna-NR_046018.2;transcript_id=somethingelse')  # transcript_id missing from attributes
+        line = sample_line(
+            attributes_str="ID=exon-NR_046018.2-1;Parent=rna-NR_046018.2;transcript_id=somethingelse"
+        )  # transcript_id missing from attributes
         expected_record = None
         parsed_record = parse_gff_record(line)
         self.assertEqual(parsed_record, expected_record)
@@ -95,7 +141,9 @@ class TestGFFParsing(unittest.TestCase):
         with self.assertRaises(ValueError) as context:
             parse_gff_record(line)
 
-        self.assertEqual(str(context.exception), "Expected 9 tab-separated fields, got 2")
+        self.assertEqual(
+            str(context.exception), "Expected 9 tab-separated fields, got 2"
+        )
 
     def test_parse_gff_record_raises_non_int_start_stop(self):
         # Raise an exception if either start or stop is not an integer
@@ -106,17 +154,19 @@ class TestGFFParsing(unittest.TestCase):
 
     def test_parse_gff_record_raises_unparseable_id(self):
         # raise an exception if we cannot parse the exon number from the ID
-        line = sample_line(attributes_str='ID=unexpected_id;Parent=rna-NR_046018.2;transcript_id=NR_046018')
+        line = sample_line(
+            attributes_str="ID=unexpected_id;Parent=rna-NR_046018.2;transcript_id=NR_046018"
+        )
         with self.assertRaises(ValueError) as context:
             parse_gff_record(line)
 
-        self.assertEqual(str(context.exception), "Failed to parse exon number from unexpected_id")
+        self.assertEqual(
+            str(context.exception), "Failed to parse exon number from unexpected_id"
+        )
 
     def test_parse_gff_file(self):
         # Test parsing the entire GFF file
-        expected_result = {
-            "rna-NR_046018.2": self.gff_records
-        }
+        expected_result = {"rna-NR_046018.2": self.gff_records}
         parsed_result = parse_gff_file(self.temp_gff.name)
         self.assertEqual(parsed_result, expected_result)
 
@@ -127,22 +177,30 @@ class TestGFFParsing(unittest.TestCase):
 
     def test_script_output(self):
         # Run the script from the command line
-        file_prefix = 'genomic_100'
-        input_gff_file = os.path.join(CURRENT_DIR, 'data', f'{file_prefix}.gff.gz')
+        file_prefix = "genomic_100"
+        input_gff_file = os.path.join(CURRENT_DIR, "data", f"{file_prefix}.gff.gz")
 
-        script_path = os.path.join(BASE_DIR, 'sbin', 'ncbi_parse_genomic_gff.py')
-        expected_output_file = os.path.join(CURRENT_DIR, 'data', f'expected_{file_prefix}.exonset.gz')
-        output_file = os.path.join(os.getcwd(), f'{file_prefix}.exonset.gz')
+        script_path = os.path.join(BASE_DIR, "sbin", "ncbi_parse_genomic_gff.py")
+        expected_output_file = os.path.join(
+            CURRENT_DIR, "data", f"expected_{file_prefix}.exonset.gz"
+        )
+        output_file = os.path.join(os.getcwd(), f"{file_prefix}.exonset.gz")
 
-        command = ['python', script_path, input_gff_file, '--prefix', file_prefix]
+        command = ["python", script_path, input_gff_file, "--prefix", file_prefix]
         subprocess.run(command, check=True)
 
         # Compare the generated output file with the expected file
         self.assertTrue(os.path.isfile(output_file), "Output file not generated.")
-        with gzip.open(expected_output_file, 'rb') as expected_file, gzip.open(output_file, 'rb') as actual_file:
+        with gzip.open(expected_output_file, "rb") as expected_file, gzip.open(
+            output_file, "rb"
+        ) as actual_file:
             expected_content = expected_file.read()
             actual_content = actual_file.read()
-            self.assertEqual(expected_content, actual_content, "Output file content doesn't match expected.")
+            self.assertEqual(
+                expected_content,
+                actual_content,
+                "Output file content doesn't match expected.",
+            )
 
         # Clean up temporary files if needed
         os.remove(output_file)
