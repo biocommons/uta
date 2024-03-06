19313db51ca8cd966d2875d85302de802c866ec4
Paige Taylor <paige.taylor@invitae.com>
2024-03-06 15:27:55 -0800

    test(IPVC-2231): add test for parsing genomic gff and file

--- sbin/ncbi_parse_genomic_gff.py
+++ sbin/ncbi_parse_genomic_gff.py
@@ -35,10 +35,12 @@ def parse_gff_record(line: str) -> Optional[GFFRecord]:
     # NC_000001.10	BestRefSeq	exon	13221	14409	.	+	.	ID=exon-NR_046018.2-3;Parent=rna-NR_046018.2;Dbxref=GeneID:100287102,Genbank:NR_046018.2,HGNC:HGNC:37102;gbkey=misc_RNA;gene=DDX11L1;product=DEAD/H-box helicase 11 like 1 (pseudogene);pseudo=true;transcript_id=NR_046018.2
 
     fields = line.strip().split('\t')
+    if len(fields) != 9:
+        raise ValueError(f'Expected 9 tab-separated fields, got {len(fields)}')
+
     seqid, source, feature, start, end, score, strand, phase, attributes_str = fields
-    if not feature == 'exon':
-        return
-    if not seqid.startswith('NC_'):
+
+    if feature != 'exon' or not seqid.startswith('NC_'):
         return
 
     attributes = {}
@@ -49,10 +51,22 @@ def parse_gff_record(line: str) -> Optional[GFFRecord]:
 
     parent_id = attributes.get('parent')
     transcript_id = attributes.get('transcript_id')
-    if not transcript_id or (not transcript_id.startswith('NM_') and not transcript_id.startswith('NR_')):
+    if not transcript_id or (not transcript_id.startswith('NM_') and not transcript_id.startswith('NR_')) or not parent_id:
         return
-    exon_number = int(attributes.get('id').split('-')[-1])
+    try:
+        exon_number = _get_exon_number_from_id(alignment_id=attributes.get('id'))
+    except (ValueError, IndexError):
+        raise ValueError(f'Failed to parse exon number from {attributes.get("id")}')
+
     return GFFRecord(seqid=seqid, start=int(start), end=int(end), strand=strand, exon_number=exon_number, parent_id=parent_id, transcript_id=transcript_id)
+
+
+def _get_exon_number_from_id(alignment_id: str) -> int:
+    """
+    Pulls the exon number from the alignment id. Expects the id to be in the format
+    exon-<transcript_id>-<exon_number>
+    """
+    return int(alignment_id.split('-')[-1])
 
 
 def parse_gff_file(file_path: str) -> dict[str, List[GFFRecord]]:
@@ -63,36 +77,42 @@ def parse_gff_file(file_path: str) -> dict[str, List[GFFRecord]]:
                 continue
             try:
                 record = parse_gff_record(line)
-            except (AttributeError, ValueError, KeyError):
-                raise Exception(f'failed at line :{line}')
+            except ValueError as e:
+                raise Exception(f'Failed at line :{line} with error: {e}')
             if record:
                 tx_data[record.parent_id].append(record)
     return {k: _sort_exons(v) for k, v in tx_data.items()}
 
 
-def main():
+def get_zero_based_exon_ranges(transcript_exons: List[GFFRecord]) -> str:
+    """Convert exon ranges to 0-based half-open format"""
+    formatted_exons = []
+    for ex in transcript_exons:
+        formatted_exons.append(','.join(map(str, (ex.start - 1, ex.end))))
+    return ';'.join(formatted_exons)
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Parse GFF file.")
-    parser.add_argument("gff_file", type=argparse.FileType('r'), help="Path to the gzipped GFF file")
+    parser.add_argument("gff_file", type=argparse.FileType('rb'), help="Path to the gzipped GFF file")
     parser.add_argument("--prefix", "-p", type=str, default="ncbi-gff", help="Output filename (default: ncbi-gff)")
     args = parser.parse_args()
 
     gff_infile = args.gff_file
-    exonset_outfile = args.prefix + "exonset.gz"
-    esw = ExonSetWriter(gzip.open(exonset_outfile + ".tmp", "wt"))
+    exonset_outfile = args.prefix + ".exonset.gz"
+    output_file = gzip.open(exonset_outfile + ".tmp", "wt")
+    esw = ExonSetWriter(output_file)
 
     transcript_alignments = parse_gff_file(gff_infile)
     logger.info(
         f"read {len(transcript_alignments)} transcript alignments from {gff_infile}")
 
     for transcript_exons in transcript_alignments.values():
-        exons_se = ';'.join(','.join(map(str, tpl)) for tpl in [(ex.start, ex.end) for ex in transcript_exons])
+        exons_se = get_zero_based_exon_ranges(transcript_exons)
         e = transcript_exons[0]
         es = ExonSet(tx_ac=e.transcript_id, alt_ac=e.seqid, method='splign', strand=e.strand, exons_se_i=exons_se)
         esw.write(es)
+    output_file.close()
     os.rename(exonset_outfile + ".tmp", exonset_outfile)
-
-
-if __name__ == "__main__":
-    main()
 
 
