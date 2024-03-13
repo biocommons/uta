f15ea0a95f452a5ca986b591a831dba2f1f2922e
Shane Giles <shane.giles@invitae.com>
2024-03-13 15:30:10 -0600

    feat(IPVC-2246): updated run-uta-build.sh script, change default database name in uta-diff, update config and readme

--- etc/scripts/run-uta-build.sh
+++ etc/scripts/run-uta-build.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-if [ "$#" -lt 1 ]
+if [ "$#" -lt 2 ]
 then
-    echo "error: too few arguments, you provided $#, 1 required"
-    echo "usage: run_uta_build_gene.sh <source_uta_v>"
+    echo "error : too few arguments, you provided $#, 2 required"
+    echo "usage: run_uta_build_gene.sh <source_uta_v> <seqrepo_data_release>"
     exit 1
 fi
 source_uta_v=$1
+seqrepo_data_release=$2
 
 set -euxo pipefail
 
 # set local variables and create working directories
-loading_schema="uta_1_1"
+loading_uta_v="uta_1_1"
 loading_dir="/temp/loading"
 dumps_dir="/temp/dumps"
 logs_dir="/temp/logs"
@@ -20,8 +21,15 @@ for d in "$loading_dir" "$dumps_dir" "$logs_dir";
 done
 
 ## Drop loading schema, and recreate
-etc/scripts/delete-schema.sh $loading_schema
-etc/scripts/create-new-schema.sh "$source_uta_v" "$loading_schema"
+etc/scripts/delete-schema.sh "$loading_uta_v"
+etc/scripts/create-new-schema.sh "$source_uta_v" "$loading_uta_v"
+
+## Load SeqRepo with new sequences
+seqrepo load -n NCBI -i "$seqrepo_data_release" \
+  /temp/refseq/H_sapiens/mRNA_Prot/human.*.rna.fna.gz \
+  /temp/refseq/H_sapiens/mRNA_Prot/human.*.protein.faa.gz \
+  /temp/genomes/refseq/vertebrate_mammalian/Homo_sapiens/all_assembly_versions/GCF_000001405*/GCF_*_genomic.fna.gz 2>& 1 | \
+  tee "$logs_dir/seqrepo-load.log"
 
 ### extract meta data
 # genes
@@ -44,8 +52,13 @@ uta --conf=etc/global.conf --conf=etc/uta_dev@localhost.conf load-geneinfo "$loa
   tee "$logs_dir/load-geneinfo.log"
 
 # transcript info
-#uta --conf=etc/global.conf --conf=etc/uta_dev@localhost.conf load-txinfo "$loading_dir/gbff.txinfo.gz" 2>&1 | \
-#  tee "$logs_dir/load-txinfo.log"
+uta --conf=etc/global.conf --conf=etc/uta_dev@localhost.conf load-txinfo "$loading_dir/gbff.txinfo.gz" 2>&1 | \
+  tee "$logs_dir/load-txinfo.log"
+
+
+### run diff
+sbin/uta-diff "$source_uta_v" "$loading_uta_v"
+
 
 ### psql_dump
-pg_dump -U uta_admin -h localhost -d uta -t "$loading_schema.gene" | gzip -c > "$dumps_dir/uta.pgd.gz"
+pg_dump -U uta_admin -h localhost -d uta -t "$loading_uta_v.gene" | gzip -c > "$dumps_dir/uta.pgd.gz"
